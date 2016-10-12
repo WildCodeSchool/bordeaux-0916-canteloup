d09bd9bff01a673fa4c229c83d9fb37c30934c86
apprenti <anneclaire.sebirou@gmail.com>
2016-10-12 13:27:28 +0200

    modification pages home tarifs et contact

--- src/js/components/contact/contact.js
+++ src/js/components/contact/contact.js
@@ -2,5 +2,6 @@
     'use strict'
     app.component("contact", {
         templateUrl: 'js/components/contact/contact.html'
+
     })
-})(angular.module('app.contact'))
+})(angular.module('app.contact'));
