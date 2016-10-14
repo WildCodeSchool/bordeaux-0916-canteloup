79284286af0a8ee4fc6d4ababb5dd43afe0ffdad
Karl Raymond Mouat <krm22@Karls-MacBook-Pro-3.local>
2016-10-14 10:14:39 +0200

    updated contact,footer,maps,sass

--- src/js/components/common/map.js
+++ src/js/components/common/map.js
@@ -7,13 +7,14 @@
                 isCollapsed: true,
                 $onInit() {
                     let map = L.map('map').setView([44.8336476, -0.5660190999999486], 13);
-                    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-                        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+                        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
                     }).addTo(map);
 
                     L.marker([44.8336476, -0.5660190999999486]).addTo(map)
-                        .bindPopup('Wild Code School.<br> 15 Place Canteloup, 33800 Bordeaux, France')
+                        .bindPopup(' L&rsquo;atielier Canteloup <br> 15 Place Canteloup, 33800 Bordeaux, France')
                         .openPopup();
+
                 }
             })
         }]
