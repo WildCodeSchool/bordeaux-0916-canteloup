9be62e4eb9beefc48a70055e03aedc72723da693
Karl Raymond Mouat <krm22@Karls-MacBook-Pro-3.local>
2016-10-17 16:05:32 +0200

    updated contact and stuff

--- src/js/components/common/contactform.js
+++ src/js/components/common/contactform.js
@@ -1,6 +1,15 @@
 ((app) => {
     'use strict'
     app.component("contactform", {
-        templateUrl: 'js/components/common/contactform.html'
+        templateUrl: 'js/components/common/contactform.html',
+        controller: [function() {
+            angular.extend(this, {
+                $onInit() {
+                },
+                saveUser() {
+                  console.log('SEND !!', this.contactInfo);
+                }
+            })
+        }]
     })
 })(angular.module('app.common'))
