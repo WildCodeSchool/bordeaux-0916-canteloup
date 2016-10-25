55ae662854bee5ef5daa1d72444f814422a003e2
apprenti <anneclaire.sebirou@gmail.com>
2016-10-25 13:26:17 +0200

    modification README & page tarif

--- src/js/components/tarifs/modal.js
+++ src/js/components/tarifs/modal.js
@@ -1,6 +1,16 @@
 ((app) => {
     'use strict'
     app.component("modal", {
-        templateUrl: 'js/components/tarifs/modal.html'
+        templateUrl: 'js/components/tarifs/modal.html',
+        // controller: function($scope, $http) {
+        //   $http.get('infoRoom.json')
+        //     .then(function(res) {
+        //         $scope.data = res.data
+        //     })
+        //     .catch(function(err) {
+        //         console.log(err)
+        //     })
+        // }
     })
+
 })(angular.module('app.common'))
