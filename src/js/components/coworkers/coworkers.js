42fc6e8397fdecad6554c83033e2a8b82d5d2063
apprenti <anneclaire.sebirou@gmail.com>
2016-11-02 17:29:27 +0100

    modif style des header et page tarif

--- src/js/components/coworkers/coworkers.js
+++ src/js/components/coworkers/coworkers.js
@@ -2,5 +2,13 @@
     'use strict'
     app.component("coworkers", {
         templateUrl: 'js/components/coworkers/coworkers.html'
+        // ,
+        // $http.get('coworkers.json').then(function(res) {
+        //     this.coworker = res.data
+        // }).catch(function(err) {
+        //     console.log(err)
+        // })
+
+
     })
 })(angular.module('app.coworkers'))
