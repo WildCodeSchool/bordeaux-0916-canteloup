6a4e1999a0dd0fcb5de4f2545779938505b232e6
Karl Raymond Mouat <krm22@Karls-MacBook-Pro-2.local>
2016-11-04 14:22:27 +0100

    sorry but fixed map scroll while working on dev

--- src/js/components/common/map.js
+++ src/js/components/common/map.js
@@ -10,7 +10,10 @@
             angular.extend(this, {
                 isCollapsed: true,
                 $onInit() {
-                    let map = L.map('map').setView([44.8336476, -0.5660190999999486], 13);
+                    let map = L.map('map',{
+                      center:[44.8336476, -0.5660190999999486],
+                      zoom:13,
+                      scrollWheelZoom: false});
                     L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
                         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
                     }).addTo(map);
